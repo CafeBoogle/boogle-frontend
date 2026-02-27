0ff0873b08a2340143df8a0aa6d5698a3442002f
xxj15 <seojini00@naver.com>
2026-02-27 19:45:08 +0900

    헤더 드롭다운 UI 개선 및 레이아웃 구조 변경

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -13,33 +13,29 @@ export default function Header() {
         onClick={() => navigate('/')}
       >
         <img src={mainLogo} alt="Boogle Logo" className="h-7 ml-3" />
-        <span className="text-lg font-bold">Boogle</span>
+        <span className="text-lg font-bold text-[#30251c]">Boogle</span>
       </div>
       <div className="relative">
         {user ? (
           <div className="relative group">
-            <span className="cursor-pointer text-l mr-5 font-semibold">
-              {user.name}님
+            <span className="cursor-pointer text-l mr-5 font-semibold text-[#30251c]">
+              {user.name} 님
             </span>
-
             <div
-              className="
-                absolute right-0 top-full
-                pt-2
-                w-32
-                bg-white border rounded-md shadow-md
+              className=" absolute right-0 top-full mt-2 w-36 bg-[#FDFCFB]  border border-[#E5DED8] 
+                rounded-xl shadow-xl shadow-brown-200/20 
                 opacity-0 invisible
-                group-hover:opacity-100 group-hover:visible
-              "
+                group-hover:opacity-100 group-hover:visible /* Tailwind CSS의 group 기능 사용 - 부모에 hover시 자식 요소 나타나도록 (opacity 0에서 100)*/
+                transition-all duration-200"
             >
               <button
-                className="block w-full px-4 py-2 text-left text-sm hover:bg-gray-100"
+                className="block w-full px-4 py-3 text-left text-sm text-[#4A3A2E] hover:bg-[#F2EDE9] rounded-t-xl transition-colors"
                 onClick={() => navigate('/mypage')}
               >
                 마이페이지
               </button>
               <button
-                className="block w-full px-4 py-2 text-left text-sm hover:bg-gray-100"
+                className="block w-full px-4 py-3 text-left text-sm text-[#A68966] hover:bg-[#F2EDE9] rounded-b-xl transition-colors font-medium"
                 onClick={logout}
               >
                 로그아웃
@@ -48,13 +44,13 @@ export default function Header() {
           </div>
         ) : (
           <button
-            className="px-3 py-1 border border-gray-300 rounded-md bg-white text-sm"
+            className="px-5 py-2  text-black text-sm font-bold rounded-full hover:bg-[#8E7455] hover:text-white transition-all"
             onClick={() => navigate('/loginpage')}
           >
             로그인
           </button>
         )}
-      </div>
+        </div>
     </header>
   );
 }
