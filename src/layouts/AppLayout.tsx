508278dff1fd56df442b3a618c48f1b7841c61f4
xxj15 <seojini00@naver.com>
2026-03-09 11:47:30 +0900

    AppLayout.tsx 수정

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,7 +1,7 @@
-// 화면 프레임 / 모바일 폭 / 공통 UI 제공하는 역할 
+// 화면 프레임 / 모바일 폭 / 공통 UI 제공하는 역할
 
-import type { ReactNode } from 'react';
-import Header from './Header';
+import type { ReactNode } from "react";
+import Header from "./Header";
 
 interface AppLayoutProps {
   children: ReactNode;
@@ -12,7 +12,7 @@ export default function AppLayout({ children }: AppLayoutProps) {
     <div className="min-h-screen w-full flex justify-center bg-[#EEE9E5] ">
       <div className="w-full max-w-[500px] min-h-screen bg-[#EEE9E5]  overflow-x-hidden">
         <Header />
-        {children}
+        <main>{children}</main>
       </div>
     </div>
   );
