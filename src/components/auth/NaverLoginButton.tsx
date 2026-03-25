7a8e47ce067ad95784d279286058095843030897
박준서 <westjun01@gmail.com>
2026-03-25 17:53:29 +0900

    Update NaverLoginButton.tsx

--- src/components/auth/NaverLoginButton.tsx
+++ src/components/auth/NaverLoginButton.tsx
@@ -5,29 +5,11 @@ declare global {
     naver: any;
   }
 }
-<<<<<<< HEAD
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
-function NaverLoginButton() {
-  useEffect(() => {
-    if (window.naver) {
-      const naverLogin = new window.naver.LoginWithNaverId({
-        clientId: import.meta.env.VITE_NAVER_CLIENT_ID,
-        callbackUrl: `${API_BASE_URL}/api/oauth/naver/callback`, // 하드코딩 제거!
-        isPopup: false,
-        loginButton: { color: 'green', type: 3, height: 50 },
-      });
 
-      naverLogin.init();
-    }
-  }, []);
-
-=======
-
-const NAVER_AUTH_URL = 'http://localhost:8080/api/oauth/naver'
+const NAVER_AUTH_URL = 'http://43.200.174.78:8080/api/oauth/naver'
 
 function NaverLoginButton() {
   // 이거 백엔드에서 code랑 status를 받고 쿠키에 저장해야해서 리다이렉트형식으로 변경함.
->>>>>>> 3938ecc (지도추가, 카페 리스트, 찜하기 api 연결 (#39))
   const handleNaverLogin = () => {
     window.location.href = NAVER_AUTH_URL;
   };
