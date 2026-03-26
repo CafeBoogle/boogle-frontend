16678d12ddb29b17043930c6e4e4079e40e3901a
박준서 <westjun01@gmail.com>
2026-03-26 15:29:52 +0900

    Update AuthContext.tsx

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -41,22 +41,6 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
   const login = (user: User) => setUser(user);
 
   const logout = async () => {
-<<<<<<< HEAD
-    try {
-      // 1. (선택사항) 우리 서버에 로그아웃 알림 (쿠키 삭제는 백엔드 redirect 시 수행되므로 생략 가능하나 안전상 호출)
-      // await axiosInstance.post('/api/user/logout');
-
-      // 2. 백엔드에 새로 만들 카카오 로그아웃 엔드포인트로 브라우저 이동
-      // 이렇게 하면 [백엔드 로직] -> [카카오 로그아웃 주소] -> [카카오 세션 종료] -> [우리 서비스 복귀]가 한 번에 일어납니다.
-      window.location.href = 'http://localhost:8080/api/oauth/kakao/logout';
-
-      // 상태 초기화
-      setUser(null);
-    } catch (err) {
-      console.error('로그아웃 과정 중 오류 발생', err);
-    }
-  };
-=======
     if (!user) return; // 로그인 안 되어 있으면 ㄴㄴ
   try {
     // 1. (선택사항) 우리 서버에 로그아웃 알림 (쿠키 삭제는 백엔드 redirect 시 수행되므로 생략 가능하나 안전상 호출)
@@ -75,7 +59,6 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
     console.error("로그아웃 과정 중 오류 발생", err);
   }
 };
->>>>>>> 3938ecc (지도추가, 카페 리스트, 찜하기 api 연결 (#39))
 
   return (
     <AuthContext.Provider value={{ user, loading, login, logout, checkAuth }}>
