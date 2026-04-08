5e41411e123fd5dc1cd4a610f15282d652e7631a
박준서 <westjun01@gmail.com>
2026-04-08 17:02:13 +0900

    Chore: 로컬에서도 서버로 연결되도록 URL 변경

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -2,7 +2,7 @@ import axios, { AxiosInstance } from 'axios';
 
 const axiosInstance: AxiosInstance = axios.create({
   // 환경 변수에서 가져오고, 없으면 기본값으로 localhost 사용
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080',
+  baseURL: import.meta.env.VITE_API_BASE_URL || 'https://www.api.moonsunpower.com/boogle',
   withCredentials: true,
 });
 export default axiosInstance; // 이제 이름이 일치합니다.
