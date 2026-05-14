b6e36a76282c867701a69da2f74d3ce5e0f201b4
박준서 <westjun01@gmail.com>
2026-05-14 13:50:14 +0900

    axios에 refresh 인증 추가

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,9 +1,35 @@
 import axios, { AxiosInstance } from 'axios';
 // 'http://localhost:8080'
-const axiosInstance: AxiosInstance = axios.create({
-  // 환경 변수에서 가져오고, 없으면 기본값으로 localhost 사용
-  baseURL: import.meta.env.VITE_API_BASE_URL || 'https://www.api.moonsunpower.com/boogle',
-  //baseURL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080/boogle',
-  withCredentials: true,
-});
+axiosInstance.interceptors.response.use(
+  (res) => res,
+  async (error) => {
+    const originalRequest = error.config;
+
+    // refresh 요청이면 무시
+    if (originalRequest.url?.includes('/auth/refresh')) {
+      return Promise.reject(error);
+    }
+
+    // 토큰 만료일 때만 refresh
+    const errorMessage = error.response?.data;
+
+    if (
+      error.response?.status === 401 &&
+      errorMessage === "TOKEN_EXPIRED" &&
+      !originalRequest._retry
+    ) {
+      originalRequest._retry = true;
+
+      try {
+        await axiosInstance.post('/auth/refresh');
+        return axiosInstance(originalRequest);
+      } catch (e) {
+        window.location.href = '/login';
+        return Promise.reject(e);
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
 export default axiosInstance; // 이제 이름이 일치합니다.
